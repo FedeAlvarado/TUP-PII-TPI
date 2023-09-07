ec8ebf12db3a4285bd21cf48e1f19f4272d3cb26
agustincoronel1 <agustincoronelb@gmail.com>
2023-09-06 21:42:07 -0300

    Opciones 1,2,3 y 4 (con comentarios)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Trabajo Práctico I - Programación II
-
+#Alvarado, Federico
+#Coronel, Agustin
 
 import os
+import bibloteca as biblioteca
 
 print("Bienvenido!")
 respuesta = ''
@@ -11,24 +13,24 @@ def menu():
     print("2 - Gestionar Devolucion")
     print("3 - Registrar nuevo libro")
     print("4 - Elimiar ejemplar")
-    print("5 - Mostrar ejemplares perstados")
+    print("5 - Mostrar ejemplares prestados")
     print("6 - Salir")
 
 while respuesta != "salir":
     menu()
     opt = input("\n Ingrese la opción de menú: ")
-    os.system ("cls") 
+    os.system ("cls") #Limpiar pantalla
     if opt.isnumeric():
         if int(opt) == 1:
-            input("Ingrese el codigo del libro")
+            biblioteca.prestar_ejemplar_libro()
         elif int(opt) == 2:
-            #completar
+            biblioteca.devolver_ejemplar_libro()
             print()
         elif int(opt) == 3:
-            #completar
+            biblioteca.registrar_nuevo_libro()
             print()
         elif int(opt) == 4:
-            #completar
+            biblioteca.eliminar_ejemplar_libro()
             print()
         elif int(opt) == 5:
             #completar
@@ -39,6 +41,6 @@ while respuesta != "salir":
     else: 
         print("Ingrese una opción numérica")
     
-    input("Presione cualquier tecla para continuar....") # Pausa
+input("Presione cualquier tecla para continuar....") # Pausa
 
 print("Hasta luego!.")
